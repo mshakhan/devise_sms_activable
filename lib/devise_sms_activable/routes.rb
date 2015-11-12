7983359e07fcfed74856d6e44640698af8f0fba4
mshakhan <mshakhan@gmail.com>
2015-11-12 21:55:07 +0300

    Rails 4 exception fix

--- lib/devise_sms_activable/routes.rb
+++ lib/devise_sms_activable/routes.rb
@@ -1,11 +1,11 @@
 module ActionDispatch::Routing
   class Mapper
-    
+
     protected
       def devise_sms_activation(mapping, controllers)
-        resource :sms_activation, :only => [:new, :create], :path => mapping.path_names[:sms_activation], :controller => controllers[:sms_activations] do
-          match :consume, :path => mapping.path_names[:consume], :as => :consume
-          get :insert, :path => mapping.path_names[:insert], :as => :insert
+        resource :sms_activation, only: [:new, :create], path: mapping.path_names[:sms_activation], controller: controllers[:sms_activations] do
+          match :consume, path: mapping.path_names[:consume], as: :consume, via: :all
+          get :insert, path: mapping.path_names[:insert], as: :insert
         end
       end
 
